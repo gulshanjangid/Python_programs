b7d92fd52ac4f1bfcb0ead52349bab31b8b69893
gulshan <gulshanjangid333@gmail.com>
2026-02-04 23:01:39 +0530

    done

---
+++ Swap_nums.py
@@ -0,0 +1,21 @@
+# a= 10
+# b= 20
+
+# print("Before swapping:")
+# print("a =", a)
+# print("b =", b)
+
+# temp  = a
+# a = b
+# b = temp
+# print("After swapping:")
+# print("a =", a)
+# print("b =", b)
+
+#swap two variables without temp variable.
+a= 10
+b= 20
+a,b = b,a
+print("After swapping:")
+print("a =", a)
+print("b =", b)
