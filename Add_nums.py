931f1d0b80e14d3559ae24d6ad62d7bac534d34c
gulshan <gulshanjangid333@gmail.com>
2026-02-04 22:33:46 +0530

    Python  programs

---
+++ Add_nums.py
@@ -0,0 +1,5 @@
+nums1 = int(input("Enter first number: "))
+nums2 = int(input("Enter second number: "))
+result = nums1 + nums2
+print("The sum is:", result)
+print(f"sum: {nums1} + {nums2} = {result}")
